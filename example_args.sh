b2671af871a3871d13f7073eb6744c2e26a7c1c1
cristian.munoz <crisstrink@gmail.com>
2019-09-12 12:53:25 -0300

    example how to pass arguments to bash script

---
+++ example_args.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Reading the options
+TEMP=`getopt -o u:p:h:g: --long user:,password:,home:,group: -- "$@"`
+
+eval set -- "$TEMP"
+
+# Extractiong options and their arguments into variables
+
+while true ; do
+        case "$1" in
+                -u|--user)
+                        USERNAME=$2 ; shift 2 ;;
+                -p|--password)
+                        PASSWORD=$2 ; shift 2 ;;
+                -h|--home)
+                        HOMEPATH=$2 ; shift 2 ;;
+                -g|--group)
+                        GROUPNAMES=$2 ; shift 2 ;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
+        esac
+done
+
+echo "USERNAME: $USERNAME"
+echo "PASSWORD: $PASSWORD"
+echo "HOME: $HOMEPATH"
+echo "GROUPNAMES: $GROUPNAMES"
